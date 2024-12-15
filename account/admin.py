01fc3bb4e17e7af189f6f4bf625797f9b44b27fa
vishnupriyan <priyanvishnu15@gmail.com>
2024-12-15 17:34:42 +0530

    added opening in admin

--- account/admin.py
+++ account/admin.py
@@ -3,27 +3,27 @@ from account.models import *
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 class UserModelAdmin(BaseUserAdmin):
-  # The fields to be used in displaying the User model.
-  # These override the definitions on the base UserModelAdmin
-  # that reference specific fields on auth.User.
-  list_display = ('id', 'email', 'name', 'tc', 'is_admin')
-  list_filter = ('is_admin',)
-  fieldsets = (
-      ('User Credentials', {'fields': ('email', 'password')}),
-      ('Personal info', {'fields': ('name', 'tc')}),
-      ('Permissions', {'fields': ('is_admin',)}),
-  )
-  # add_fieldsets is not a standard ModelAdmin attribute. UserModelAdmin
-  # overrides get_fieldsets to use this attribute when creating a user.
-  add_fieldsets = (
-      (None, {
-          'classes': ('wide',),
-          'fields': ('email', 'name', 'tc', 'password1', 'password2'),
-      }),
-  )
-  search_fields = ('email',)
-  ordering = ('email', 'id')
-  filter_horizontal = ()
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserModelAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('id', 'email', 'name', 'tc', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        ('User Credentials', {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'tc')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserModelAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'tc', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email', 'id')
+    filter_horizontal = ()
 
 
 # Now register the new UserModelAdmin...
@@ -32,3 +32,4 @@ admin.site.register(Income)
 admin.site.register(Expense)
 admin.site.register(IncomeType)
 admin.site.register(ExpenseType)
+admin.site.register(Opening)
