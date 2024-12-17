444c22c01423ed3fd8bb716d18f8d27e18148845
vishnupriyan <priyanvishnu15@gmail.com>
2024-12-17 19:46:53 +0530

    Added scripts for deployment

--- UserData.sh
+++ UserData.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Replace {YOUR_GIT_REOPO_URL} with your actual Git repository URL
-GIT_REPO_URL="https://github.com/codewithmuh/django-aws-ec2-nginx-gunicorn.git"
+GIT_REPO_URL="https://github.com/vishnu2646/Paymet-Recipts-Backend.git"
 
 # If using Private Repo
 #GIT_REPO_URL="https://<your_username>:<your_PAT>@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
 
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
-PROJECT_MAIN_DIR_NAME="django-aws-ec2-nginx-gunicorn"
+PROJECT_MAIN_DIR_NAME="Paymet-Recipts-Backend"
 
 # Clone repository
 git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
