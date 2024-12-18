0d83c64def814869017e343f571aa9b79d99c9ef
vishnupriyan <priyanvishnu15@gmail.com>
2024-12-18 18:55:11 +0530

    removed static

--- djangoauthapi1/settings.py
+++ djangoauthapi1/settings.py
@@ -135,13 +135,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-MEDIA_URLS = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
